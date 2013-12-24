3ddfdfea947eb20244b7f8406874939f1b050859
Ilan Frumer <ilanfrumer@gmail.com>
2013-12-24 15:26:35 +0200

    Update terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -1,3 +1,4 @@
+sudo apt-get install -y zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 sudo chsh -s $(which zsh) $USER
 git clone https://github.com/rupa/z.git ~/z
