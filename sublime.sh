54adbb9e324f10c55029ca37c48331d76a47cbd6
Ilan Frumer <ilanfrumer@gmail.com>
2014-03-03 11:18:31 +0200

    Update sublime.sh

--- sublime.sh
+++ sublime.sh
@@ -2,5 +2,8 @@ sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install -y sublime-text-3
 wget -xNP ~/.config/sublime-text-3/Installed\ Packages/ http://sublime.wbond.net/Package%20Control.sublime-package
-curl -L "https://raw.github.com/IlanFrumer/linux-enviroment/master/files/Preferences.sublime-settings" > ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
-curl -L "https://raw.github.com/IlanFrumer/linux-enviroment/master/files/Package%20Control.sublime-settings" > ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
+mkdir -p ~/.config/sublime-text-3/Packages/User/
+cd ~/.config/sublime-text-3/Packages/User/
+wget -N "https://raw.github.com/IlanFrumer/linux-enviroment/master/files/Preferences.sublime-settings"
+wget -N "https://raw.github.com/IlanFrumer/linux-enviroment/master/files/Package%20Control.sublime-settings"
+cd
