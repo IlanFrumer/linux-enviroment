c0c109410339dedbe921f156228f36eb135279b0
Ilan Frumer <ilanfrumer@gmail.com>
2014-03-10 01:17:11 +0200

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,4 +1,4 @@
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install -y node.js
-sudo npm install -g bower grunt-cli coffee-script karma express hexo js2coffee nodemon
+sudo npm install -g node-gyp bower grunt-cli coffee-script karma express js2coffee nodemon jade html2jade
